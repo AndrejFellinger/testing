9a97794bd238f674e41641c227885c2f53ab34da
Andrej Fellinger <fellinger.andrej2gmail.com>
2023-10-21 14:18:04 +0200

    update

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,23 +1,27 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
-    </ion-content>
-  </ion-page>
+  <Hero :data="hero" />
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import Hero from '@/components/HeroComp.vue';
+
+
+</script>
+
+<script>
+  export default{
+  data() {
+    return {
+      hero: [
+        {
+          url: "https://tinyhousetalk.com/wp-content/uploads/400-sq-ft-cabin-by-wheelhaus-the-wedge-00011.jpg",
+          badgeTitle: "Článok",
+          title: "Inovatívny startup prináša cenovo dostupné montované chatky",
+          hashtags: ["inovacie", "biznis", "startupy", "fyp"]
+        }
+      ],
+    }
+  },
+}
 </script>
